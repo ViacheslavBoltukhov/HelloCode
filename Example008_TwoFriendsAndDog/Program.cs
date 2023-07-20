4fe6fb4ee546a6489c3deb8da6c16da4a75896ca
ViacheslavBoltukhov <mr.boltukhov@mail.ru>
2023-07-20 12:39:23 +0300

    Added Example008 two friends and dog

---
+++ Example008_TwoFriendsAndDog/Program.cs
@@ -0,0 +1,28 @@
+﻿int Count = 0;
+Console.Write("Enter the distance between friends: ");
+double Distance = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter the speed first friend: ");
+int FirstFriendSpeed = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the speed second friend: ");
+int SecondFriendSpeed = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the speed dog: ");
+int DogSpeed = Convert.ToInt32(Console.ReadLine());
+int Friend = 2;
+double Time = 1;
+while (Distance>10)
+{
+    if (Friend == 1)
+    {
+        Time = Distance/(FirstFriendSpeed+DogSpeed);
+        Friend = 2;
+    }
+    if (Friend == 2)
+    {
+        Time = Distance/(SecondFriendSpeed+DogSpeed);
+        Friend = 1;
+    }
+    Distance = Distance - (FirstFriendSpeed + SecondFriendSpeed)*Time;
+    Count++;
+}
+Console.WriteLine(Distance);
+Console.WriteLine(Count);

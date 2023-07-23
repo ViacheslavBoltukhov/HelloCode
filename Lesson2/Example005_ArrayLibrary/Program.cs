7959b444f4635f8c1e6f94d8f8a10e84b1d5123b
ViacheslavBoltukhov <mr.boltukhov@mail.ru>
2023-07-23 13:30:16 +0300

    Added in Lesson2 Example005 Array library

---
+++ Lesson2/Example005_ArrayLibrary/Program.cs
@@ -0,0 +1,28 @@
+﻿void FillArray(int[] collettion)
+{
+    int lenght = collettion.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        collettion[index] = new Random().Next(1,10);
+        index++;
+    }
+}
+
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int possition = 0;
+    while (possition < count)
+    {
+        Console.WriteLine(col[possition]);
+        possition++;
+    }
+}
+
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
